3a17371a12b7bd74b86f56568a65410386ec4de4
yarimatusevich <yari.matusevich@gmail.com>
2026-08-10 15:14:32 -0400

    Create User and Company data models

---
+++ models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+    phone_numer: str
+    state: str
+    on_dnc: bool
+
+class Company(BaseModel):
+    name: str
+    known_aliases: list[str]
+    address: str
+    created_at: datetime
+
