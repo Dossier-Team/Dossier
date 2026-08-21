ff37e67a4a364dd6904a0b1effa663b30c6c496c
yarimatusevich <yari.matusevich@gmail.com>
2026-08-21 15:27:51 -0400

    Rename AGENT_ID to ELEVENLABS_AGENT_ID for clarity.

--- src/client.py
+++ src/client.py
@@ -7,4 +7,4 @@ load_dotenv()
 
 elevenlabs = ElevenLabs(api_key=os.environ['ELEVENLABS_API_KEY'])
 ELEVENLABS_WEBHOOK_SECRET = os.environ['ELEVENLABS_WEBHOOK_SECRET']
-AGENT_ID = os.environ['ELEVENLABS_AGENT_ID']
+ELEVENLABS_AGENT_ID = os.environ['ELEVENLABS_AGENT_ID']
