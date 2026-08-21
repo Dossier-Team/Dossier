583bc8b3c8eef0c9f91c2a3a44f04ff9263bc10b
yarimatusevich <yari.matusevich@gmail.com>
2026-08-21 15:26:19 -0400

    Load all environmental variables in this module so all other dependent modules can import the same instance of each.

---
+++ src/client.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+from elevenlabs import ElevenLabs
+
+load_dotenv()
+
+elevenlabs = ElevenLabs(api_key=os.environ['ELEVENLABS_API_KEY'])
+ELEVENLABS_WEBHOOK_SECRET = os.environ['ELEVENLABS_WEBHOOK_SECRET']
+AGENT_ID = os.environ['ELEVENLABS_AGENT_ID']
