5429e2c9671e6d8d283127f0bf8ccbc93f66c5db
Yaroslav Matusevich <yari.matusevich@gmail.com>
2026-08-31 22:45:15 -0400

    Remove tools router registration from server

--- server.py
+++ server.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 
 from src.routes.elevenlabs_routes import router as elevenlabs_router
-from src.routes.tools_routes import router as tools_router
 from src.routes.twilio_routes import router as twilio_router
+
 app = FastAPI()
 app.include_router(elevenlabs_router)
 app.include_router(twilio_router)
-app.include_router(tools_router)
-
 
 if __name__ == "__main__":
     import uvicorn
