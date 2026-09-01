743268fec2304265b9e9ed761b40590c97081ae3
Yaroslav Matusevich <yari.matusevich@gmail.com>
2026-08-31 21:55:53 -0400

    Delete standalone LLM module in favor of creating an instance of chosen LLM model in clients module and importing where required

--- src/clients.py
+++ src/clients.py
@@ -2,9 +2,14 @@ import os
 
 from dotenv import load_dotenv
 from elevenlabs import ElevenLabs
+from langchain.chat_models import init_chat_model
+
 
 load_dotenv()
 
 elevenlabs = ElevenLabs(api_key=os.environ['ELEVENLABS_API_KEY'])
 ELEVENLABS_WEBHOOK_SECRET = os.environ['ELEVENLABS_WEBHOOK_SECRET']
 ELEVENLABS_AGENT_ID = os.environ['ELEVENLABS_AGENT_ID']
+
+GEMINI_API_KEY = os.environ['GEMINI_API_KEY']
+LLM_MODEL = init_chat_model("google_genai:gemini-3.7-flash")
