79c29401481956ea3dded96de2d0e96d71425403
Yaroslav Matusevich <yari.matusevich@gmail.com>
2026-09-01 19:50:57 -0400

    Fix mismatched qoute types (whole file is now using single qoutes)

--- src/clients.py
+++ src/clients.py
@@ -12,4 +12,4 @@ ELEVENLABS_WEBHOOK_SECRET = os.environ['ELEVENLABS_WEBHOOK_SECRET']
 ELEVENLABS_AGENT_ID = os.environ['ELEVENLABS_AGENT_ID']
 
 GEMINI_API_KEY = os.environ['GEMINI_API_KEY']
-LLM_MODEL = init_chat_model("google_genai:gemini-3.7-flash")
+LLM_MODEL = init_chat_model('google_genai:gemini-3.7-flash')
