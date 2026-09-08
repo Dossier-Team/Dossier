066b719a3006c615198ccbf6ecac5eb3d5147f51
Yaroslav Matusevich <yari.matusevich@gmail.com>
2026-09-08 14:49:49 -0400

    Remove global variables storing secret keys for ElevenLabs

--- src/clients.py
+++ src/clients.py
@@ -1,15 +1,11 @@
 import os
 
 from dotenv import load_dotenv
-from elevenlabs import ElevenLabs
 from langchain.chat_models import init_chat_model
 
 
 load_dotenv()
 
-elevenlabs = ElevenLabs(api_key=os.environ['ELEVENLABS_API_KEY'])
-ELEVENLABS_WEBHOOK_SECRET = os.environ['ELEVENLABS_WEBHOOK_SECRET']
-ELEVENLABS_AGENT_ID = os.environ['ELEVENLABS_AGENT_ID']
-
 GEMINI_API_KEY = os.environ['GEMINI_API_KEY']
+PUBLIC_HOST_NAME = os.environ['PUBLIC_HOST_NAME']
 LLM_MODEL = init_chat_model('google_genai:gemini-3.7-flash')
