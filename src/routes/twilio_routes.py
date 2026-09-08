4c267ca02aed603396c739ee77509faff08b8289
Yaroslav Matusevich <yari.matusevich@gmail.com>
2026-09-08 15:13:39 -0400

    Remove endpoint for passing TwiML to ElevenLabs in favor of endpoint for doing the same for Deegram

--- src/routes/twilio_routes.py
+++ src/routes/twilio_routes.py
@@ -3,21 +3,33 @@ from src.clients import elevenlabs, ELEVENLABS_AGENT_ID
 
 router = APIRouter()
 
-@router.post('/twilio/inbound')
-async def handle_inbound_call(request: Request) -> Response:
-    """
-    When Twilio receives an inbound call passes the call to the ElevenLabs Agent
-    """
+@router.post("/twiml")
+async def twiml(request: Request) -> Response:
+    xml = f"""<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Connect>
+    <Stream url="wss://{PUBLIC_HOSTNAME}/media" />
+  </Connect>
+</Response>"""
+    return Response(content=xml, media_type="application/xml")
 
-    form = await request.form()
-    from_number = form.get('From')
-    to_number = form.get('To')
 
-    twiml = elevenlabs.conversational_ai.twilio.register_call(
-        agent_id=ELEVENLABS_AGENT_ID,
-        from_number=from_number,
-        to_number=to_number,
-        direction='inbound',
-    )
-
-    return Response(content=twiml, media_type='application/xml')
+# @router.post('/twilio/inbound')
+# async def handle_inbound_call(request: Request) -> Response:
+#     """
+#     Opens a bidirectional WebSocket allowing agent to receive call audio
+#     and for agent's response to be received back
+#     """
+#
+#     form = await request.form()
+#     from_number = form.get('From')
+#     to_number = form.get('To')
+#
+#     twiml = elevenlabs.conversational_ai.twilio.register_call(
+#         agent_id=ELEVENLABS_AGENT_ID,
+#         from_number=from_number,
+#         to_number=to_number,
+#         direction='inbound',
+#     )
+#
+#     return Response(content=twiml, media_type='application/xml')
